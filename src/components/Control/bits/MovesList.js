512063796de2de8eea6adb035c2c43f9ec8d41dc
m-arslaan1 <mohd.arslaan49@gmail.com>
2024-12-21 13:10:00 +0530

    addition in movelist

--- src/components/Control/bits/MovesList.js
+++ src/components/Control/bits/MovesList.js
@@ -6,6 +6,7 @@ const MovesList = () => {
     const { appState : {movesList} } = useAppContext();
 
     return <div className='moves-list'>
+        <h2>Moves List</h2>
         {movesList.map((move,i) => 
             <div key={i} data-number={Math.floor(i/2)+1}>{move}</div>
         )}
